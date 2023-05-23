27db364a7d1102eca2ea643e0e82ff2c950988ad
Alem Tuzlak <t.zlak@hotmail.com>
2023-05-23 17:15:29 +0200

    Finished the v1 to v2 conversion script

--- src/commands/flattenRoutes.ts
+++ src/commands/flattenRoutes.ts
@@ -1,83 +1,52 @@
 import * as vscode from "vscode";
+import { addUnderscoreToFirstPart, updateFileDependencies } from "../utils/dependencies";
+import { commandWithLoading } from "../utils/vscode";
+
 export async function flattenRoutes(rootPath: string) {
-  const tempFolder = vscode.Uri.joinPath(vscode.Uri.file(rootPath), "temp");
-  await vscode.workspace.fs.createDirectory(tempFolder);
-
-  await flattenRoutesSubpaths(rootPath, rootPath, tempFolder);
-  await flattenRoutesSubpaths(rootPath, rootPath, tempFolder);
-
-  // Remove the temporary folder
-  await vscode.workspace.fs.delete(tempFolder, { recursive: true });
+  const confirmation = await vscode.window.showInputBox({
+    prompt: "Please confirm you want to do this and you have a backup in case you run into issues by typing YES",
+  });
+  /**
+   *   const tempFolderPath = rootPath.replace("routes", "tmp");
+  const rootPathUri = vscode.Uri.file(rootPath);
+  const tempPathUri = vscode.Uri.file(tempFolderPath);
+   */
+  if (confirmation !== "YES") {
+    return await vscode.window.showErrorMessage("You must confirm you have a backup and you are sure.");
+  }
+  await commandWithLoading("Flattening routes to v2 convention", () => flattenRoutesSubpaths(rootPath, rootPath));
 }
-const addUnderscoreToFirstPart = (str: string = ""): string => {
-  const parts = str.split(".");
-  if (parts.length > 1) {
-    parts[0] = parts[0] + "_";
-  }
-  return parts.join(".");
-};
 
-const convertDependencyPath = (dependencyPath: string) => {
-  const convertedPath = dependencyPath.replace(".tsx", "").replace(".jsx", "").replace(/\//g, ".");
-  return convertedPath;
-};
-
-const convertRoutePathToTildaFromRelative = (routePath: string) => {
-  const parentPath = "../".repeat(routePath.split("/").length - 1);
-  const convertedPath = routePath.replace(/^\.\.\//, "");
-  return `${parentPath}routes/${convertedPath}`;
-};
-
-const updateFileDependencies = async (filePath: vscode.Uri, newFileName: string) => {
-  // Read the contents of the file
-  const fileContents = await vscode.workspace.fs.readFile(filePath);
-  const fileText = fileContents.toString();
-
-  // Regular expression to match import statements
-  const importRegex = /import\s.*?\sfrom\s['"](.*?)['"]/g;
-
-  // Find all import statements in the file
-  let match;
-  while ((match = importRegex.exec(fileText)) !== null) {
-    const importStatement = match[0];
-    const dependencyPath = match[1];
-    console.log(importStatement, dependencyPath);
-
-    console.log(convertDependencyPath(convertRoutePathToTildaFromRelative(dependencyPath)));
-    // Check if the dependency path refers to the file being renamed
-    if (dependencyPath === newFileName) {
-      // Update the import statement with the new file name
-      //const updatedImportStatement = importStatement.replace(dependencyPath, newFileName);
-      //fileText = fileText.replace(importStatement, updatedImportStatement);
-    }
-  }
-
-  // Write the updated contents back to the file
-  //await vscode.workspace.fs.writeFile(filePath, Buffer.from(fileText));
-};
-
-const flattenRoutesSubpaths = async (root: string, path: string, tempFolder: vscode.Uri) => {
+const flattenRoutesSubpaths = async (root: string, path: string) => {
   try {
+    // Get all files in the current directory
     const files = await vscode.workspace.fs.readDirectory(vscode.Uri.file(path));
+
     for (const [file, fileType] of files) {
       const filePath = vscode.Uri.joinPath(vscode.Uri.file(path), file);
 
       if (fileType === vscode.FileType.File) {
         const fileParts = file.split("\\");
         const fileName = fileParts.pop();
+        // Check if the route is suffixed
         const withSuffix = files.some(
           ([file, fileType]) =>
+            // Check if there is a directory with the same name (eg. /admin directory and this is an admin.tsx route) in the current directory
             file.startsWith(fileName?.split(".")[0].replace("__", "").replace("_", "") || "") &&
             fileType === vscode.FileType.Directory
         );
+        //We take the path from the routes directory (eg c:/remix-app/app/routes/dir => dir) and we process the folder name
         const folderName = path.split("routes")[1].replace("\\", "").split("\\").join(".").replace(/__/g, "_");
+        // We add the suffix if the route is escaped
         const processedFileName = withSuffix ? addUnderscoreToFirstPart(fileName) : fileName;
+        // We convert the file name to v2 from v1 by using the path to the file + the transforms from v1 to v2 conventions
         const newFileName =
           (folderName.length ? folderName + "." : "") +
           processedFileName!.replace(/__/g, "_").replace(/^index\./, "_index.");
 
         const newFilePath = vscode.Uri.joinPath(vscode.Uri.file(root), newFileName);
-        //const newPath = vscode.Uri.joinPath(vscode.Uri.file(root), newFilePath.path);
+
+        // If this route already exists we do nothing
         try {
           const fileExists = await vscode.workspace.fs.readFile(newFilePath);
           if (fileExists) {
@@ -85,32 +54,18 @@ const flattenRoutesSubpaths = async (root: string, path: string, tempFolder: vsc
           }
         } catch (err) {}
 
-        await updateFileDependencies(filePath, newFileName);
+        // We update the file dependencies before moving it
+        await updateFileDependencies(filePath, root, newFilePath.path);
+        // We move it to the root of the routes directory with the updated dependencies
         await vscode.workspace.fs.rename(filePath, newFilePath);
-        await vscode.commands.executeCommand("editor.action.organizeImports");
-        console.log(`Renamed ${file} to ${newFileName}`);
+        // If the current file is actually a directory we recursively keep generating routes
       } else if (fileType === vscode.FileType.Directory) {
-        await flattenRoutesSubpaths(root, filePath.fsPath, tempFolder);
+        await flattenRoutesSubpaths(root, filePath.fsPath);
+        // After we are done with the generation we delete the directory we are in recursively
+        await vscode.workspace.fs.delete(filePath, { recursive: true });
       }
     }
   } catch (err) {
-    console.error("Error renaming files:", err);
-
-    // Move back the old structure to the root folder
-    try {
-      const tempFiles = await vscode.workspace.fs.readDirectory(tempFolder);
-      for (const [file, fileType] of tempFiles) {
-        const tempFilePath = vscode.Uri.joinPath(tempFolder, file);
-        const newFilePath = vscode.Uri.joinPath(vscode.Uri.file(root), file);
-        await vscode.workspace.fs.rename(tempFilePath, newFilePath);
-      }
-
-      // Remove the temporary folder
-      await vscode.workspace.fs.delete(tempFolder);
-    } catch (err) {
-      console.error("Error moving back old structure:", err);
-    }
-
     return;
   }
 };
