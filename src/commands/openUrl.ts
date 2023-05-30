bc3eb9503d746288b3d8a5721814f4dea03a0478
Alem Tuzlak <t.zlak@hotmail.com>
2023-05-30 22:44:04 +0200

    update to v1.78 engine

--- src/commands/openUrl.ts
+++ src/commands/openUrl.ts
@@ -1,5 +1,5 @@
 import * as vscode from "vscode";
 
-export const openUrl = (url: string) => {
-  vscode.env.openExternal(vscode.Uri.parse(url));
+export const openUrl = async (url: string) => {
+  await vscode.env.openExternal(vscode.Uri.parse(url));
 };
